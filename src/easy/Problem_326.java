1849b464a04d2211ea9000f68db652583ee46ff0
ShahriyarHussain <shossainshihab36@gmail.com>
2022-03-03 20:42:01 +0600

    Add Problem 231,326,342

---
+++ src/easy/Problem_326.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class Problem_326 {
+
+
+    public boolean isPowerOfThree(int n) {
+        if (n == 0 ) return false;
+        while (n != 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
---
+++ src/easy/Problem_342.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class Problem_342 {
+
+    public boolean isPowerOfFour(int n) {
+        if (n == 0 ) return false;
+        while (n != 1) {
+            if (n % 4 != 0) {
+                return false;
+            }
+            n /= 4;
+        }
+        return true;
+    }
+}
