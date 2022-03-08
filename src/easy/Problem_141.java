2883a89ec46539241ddb21711df58899e9cec540
ShahriyarHussain <shossainshihab36@gmail.com>
2022-03-08 22:43:00 +0600

    Add Problem 141

---
+++ src/easy/Problem_141.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class Problem_141 {
+        /*
+    Name: Linked List Cycle
+    Difficulty: Easy
+    Link: https://leetcode.com/problems/linked-list-cycle/
+     */
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        for (ListNode fast = head, slow = head;
+             fast != null && fast.next != null;) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) { return true; }
+        }
+        return false;
+    }
+}
