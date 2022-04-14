8e5c81529221a35c157415cc124870bd18d6ff40
ShahriyarHussain <shossainshihab36@gmail.com>
2022-04-14 15:36:33 +0600

    Add Problem 703

--- src/easy/Problem_21.java
+++ src/easy/Problem_21.java
@@ -38,15 +38,5 @@ public class Problem_21 {
     }
 }
 
-/**
- Definition for singly-linked list.**/
-public class ListNode {
-    public int val;
-    public ListNode next;
-    public ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 //The ListNode Class is given in Leetcode and no need to implement it.
 //Only uncomment if testing the code in local machine
