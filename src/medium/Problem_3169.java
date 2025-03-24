cc112b13391cf4e07513f7757478f0503cc36c60
ShahriyarHussain <shossainshihab36@gmail.com>
2025-03-25 02:59:36 +0600

    Add Problem 3169

---
+++ src/medium/Problem_3169.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Set;
+
+public class Problem_3169 {
+
+    /*
+    Name: Count Days Without Meetings
+    Difficulty: Medium
+    Link: https://leetcode.com/problems/count-days-without-meetings/
+    */
+
+    public static int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+        int freeDays = meetings[0][0] - 1;
+        int day = meetings[0][1] + 1;
+
+        for (int index = 1; index < meetings.length; index++) {
+            if (day > meetings[index][0] && day > meetings[index][1]) {
+                continue;
+            }
+            if (day >= meetings[index][0] && day <= meetings[index][1]) {
+                day = meetings[index][1] + 1;
+                continue;
+            }
+            freeDays += meetings[index][0] - day;
+            day = meetings[index][1] + 1;
+        }
+        freeDays += (days - day) + 1;
+        return freeDays;
+    }
+
+}
